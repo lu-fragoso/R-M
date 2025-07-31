443b00f604f6ccab34b7ff4b80bed855bea33475
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2025-07-31 19:15:42 -0300

    feat: Adicionar rotas de autenticação e gerenciamento de senhas

--- rickmorty-app/routes/web.php
+++ rickmorty-app/routes/web.php
@@ -1,17 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('home');
-});
+use App\Http\Controllers\PersonagensController;
 
 
 Route::get('/sobre', function () {
     return view('sobre');
 });
 
-use App\Http\Controllers\PersonagensController;
+Route::get('/', [PersonagensController::class, 'index'])->name('personagens.index');
 
-Route::get('/personagens', [PersonagensController::class, 'index'])->name('personagens.index');
 Route::get('/personagem/{id}', [PersonagensController::class, 'show'])->name('personagens.show');
+
+use App\Http\Controllers\FavoritoController;
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/favoritar', [FavoritoController::class, 'store'])->name('favoritos.store');
+    Route::get('/favoritos', [FavoritoController::class, 'index'])->name('favoritos.index');
+});
+
+Route::get('/login-teste', function () {
+    return auth()->check() ? 'Logado como ' . auth()->user()->email : 'Não logado';
+});
+
+
+require __DIR__.'/auth.php';
